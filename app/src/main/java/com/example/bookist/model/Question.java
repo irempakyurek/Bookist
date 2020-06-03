da1285487757aedf32387bed7f32c2002e532661
irempakyurek <irempakyurek1@gmail.com>
2020-06-03 15:55:02 +0300

    Questions were added using firebase for the books

---
+++ app/src/main/java/com/example/bookist/model/Question.java
@@ -0,0 +1,59 @@
+package com.example.bookist.model;
+
+public class Question {
+
+    String question;
+    String optionA;
+    String optionB;
+    String optionC;
+    int correctAnswer;
+
+
+    public Question(String question, String optionA, String optionB, String optionC, int correctAnswer) {
+        this.question = question;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
